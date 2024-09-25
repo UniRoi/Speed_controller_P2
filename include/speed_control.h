9f341c13cebabb7a382feb62b50ed6b32b704860
reinto1234 <tobiasr550@gmail.com>
2024-09-25 16:24:26 +0000

    part3 basic pi controller working without virtual

--- include/speed_control.h
+++ include/speed_control.h
@@ -1,21 +1,44 @@
-#ifndef P_CONTROL_H
-#define P_CONTROL_H
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
 
-// #include <encoder.h>
+class Controller
+{
+public:
+    Controller() {}
+    virtual ~Controller() {}
+    
+    // Pure virtual function to enforce derived classes to implement this method
+    virtual double update(double ref, double actual) = 0;
+};
 
-class P_control
+class P_control : public Controller
 {
 private:
     float fKp_;
 public:
     P_control(float fKp);
     ~P_control();
-    double update(double ref, double actual);
+    
+    // Override update method
+    double update(double ref, double actual) override;
 };
 
+class PI_control : public Controller
+{
+private:
+    float fKp;    // Proportional gain
+    float fTi;    // Integral time
+    double integral; // To accumulate integral error over time
+    float dt;
+    double minOutput;
+    double maxOutput;
 
+public:
+    PI_control(float fKp_t, float fTi_t, float dt_t, double maxOutput_t, double minOutput_t);
+    ~PI_control();
 
+    // Override update method
+    double update(double ref, double actual) override;
+};
 
-
-
-#endif
+#endif // CONTROLLER_H
--- src/speed_control.cpp
+++ src/speed_control.cpp
@@ -1,8 +1,8 @@
-#include <speed_control.h>
+#include "speed_control.h"
 
-P_control::P_control(float fKp)
+// P_control class methods
+P_control::P_control(float fKp) : fKp_(fKp)
 {
-    fKp_ = fKp;
 }
 
 P_control::~P_control()
@@ -12,4 +12,43 @@ P_control::~P_control()
 double P_control::update(double ref, double actual)
 {
     return (ref - actual) * fKp_;
+}
+
+// PI_control class methods
+PI_control::PI_control(float fKp_t, float fTi_t, float dt_t, double maxOutput_t, double minOutput_t)
+    : fKp(fKp_t), fTi(fTi_t), dt(dt_t), maxOutput(maxOutput_t), minOutput(minOutput_t), integral(0.0)
+{
+}
+
+PI_control::~PI_control()
+{
+}
+
+double PI_control::update(double ref, double actual)
+{
+    // Error calculation
+    double error = ref - actual;
+
+    // Update integral
+    integral += (error * dt) / fTi;
+
+    // Proportional term
+    double P_out = fKp * error;
+
+    // Integral term
+    double I_out = fKp * integral;
+
+    // Compute raw output (without saturation)
+    double output = P_out + I_out;
+
+    // Apply saturation (windup protection)
+    if (output > maxOutput) {
+        output = maxOutput;
+        integral -= (error * dt) / fTi; // Undo integral update
+    } else if (output < minOutput) {
+        output = minOutput;
+        integral -= (error * dt) / fTi; // Undo integral update
+    }
+
+    return output;
 }
